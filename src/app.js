ec31339dd118664bf9dea89ca5fd73549a41e78c
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-12-12 20:27:28 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use("/api", routes);
