7ef5c09ff9533458e4371875983217b801c87706
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-12-13 18:25:40 +0530

    deploy

--- src/app.js
+++ src/app.js
@@ -4,11 +4,25 @@ import cors from "cors";
 
 const app = express();
 
+const allowedOrigins = [
+  "https://race-master-nu.vercel.app",
+  "http://localhost:5173", // for local dev
+];
+
 app.use(
   cors({
-    origin: "*", 
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
